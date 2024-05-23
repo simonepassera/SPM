ec9162e9e6ea0df8516c6329d3940c93fcb34998
Simone <passe98@gmail.com>
2024-05-23 14:33:06 +0200

    Update a4

---
+++ Assignment4/nkeys-mpi.cpp
@@ -0,0 +1,298 @@
+#include <cstdio>
+#include <random>
+#include <map>
+#include <vector>
+#include <mpi.h>
+#include <set>
+#include <sys/time.h>
+
+const long SIZE = 64;
+const long STREAM_SIZE = 100000;
+
+double diffmsec(const struct timeval &a, const struct timeval &b) {
+	long sec  = (a.tv_sec  - b.tv_sec);
+    long usec = (a.tv_usec - b.tv_usec);
+    
+    if(usec < 0) {
+        sec--;
+        usec += 1000000;
+    }
+
+    return ((double)(sec*1000) + (double)usec/1000.0);
+}
+
+long random(const int &min, const int &max) {
+	static std::mt19937 generator(117);
+	std::uniform_int_distribution<long> distribution(min, max);
+	return distribution(generator);
+}
+
+void init(auto &M, const long c1, const long c2, const long key) {
+	for(long i = 0; i < c1; i++)
+		for(long j = 0; j < c2; j++)
+			M[i][j] = (key-i-j) / static_cast<float>(SIZE);
+}
+
+// Matrix multiplication: C = A x B  A[c1][c2] B[c2][c1] C[c1][c1]
+// mm returns the sum of the elements of the C matrix
+auto mm(const auto& A, const auto& B, const long c1,const long c2) {
+	float sum {0};
+	
+    for (long i = 0; i < c1; i++) {
+        for (long j = 0; j < c1; j++) {
+            auto accum = float(0.0);
+            
+			for (long k = 0; k < c2; k++)
+            	accum += A[i][k] * B[k][j];
+            
+			sum += accum;
+		}
+	}
+
+	return sum;
+}
+
+// initialize two matrices with the computed values of the keys
+// and execute a matrix multiplication between the two matrices
+// to obtain the sum of the elements of the result matrix 
+float compute(const long c1, const long c2, long key1, long key2) {
+	std::vector<std::vector<float>> A(c1, std::vector<float>(c2, 0.0));
+	std::vector<std::vector<float>> B(c2, std::vector<float>(c1, 0.0));
+
+	init(A, c1, c2, key1);
+	init(B, c2, c1, key2);
+	auto r = mm(A,B, c1,c2);
+	
+	return r;
+}
+
+long findOwner(std::vector<std::set<long>> &setKeys, long key) {
+	int i = 0;
+
+	for (auto it = setKeys.begin(); it != setKeys.end(); it++, i++)
+		if (it->contains(key))
+			return i;
+	
+	return -1;
+}
+
+int main(int argc, char* argv[]) {
+	struct timeval wt1, wt0;
+	gettimeofday(&wt0, NULL);
+
+	MPI_Init(&argc, &argv);
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (argc < 3) {
+		if (!rank) {
+			std::printf("use: %s nkeys length [print(0|1)]\n", argv[0]);
+			std::printf("     print: 0 disabled, 1 enabled\n");
+		}
+
+		MPI_Abort(MPI_COMM_WORLD, -1);
+	}
+
+	// Number of processes in the group of MPI_COMM_WORLD
+	int size; 
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	long nkeys  = std::stol(argv[1]); // Total number of keys
+	long length = std::stol(argv[2]); // Stream length
+	bool print = false;
+
+	if (argc == 4)
+		print = (std::stoi(argv[3]) == 1) ? true : false;
+
+	std::vector<std::set<long>> setKeys(size);
+	std::map<long, float> V;
+
+	long block_size = nkeys / size;
+	long r = nkeys % size;
+
+	for (int i = 0; i < size; i++) {
+		long lower = (i * block_size) + (i < r ? i : r);
+		long upper = lower + block_size + (i < r ? 1 : 0);
+		
+		for (int k = lower; k < upper; k++) {
+			setKeys[i].insert(k);
+			if (i == rank) V[k] = 0;
+		}
+	}
+
+	std::vector<long> map(nkeys, 0);
+	long key1, key2, newKey1, count;
+	long sequence[STREAM_SIZE*2];
+	long sequence_size;
+	long total_sequence = 0;
+	float r1 = 0, r2 = 0;
+	bool resetkey1 = false, resetkey2 = false;
+
+	MPI_Request ibcast_request;
+	std::vector<MPI_Request> request_key1(size-1);
+	std::vector<MPI_Request> request_key2(size-1);
+
+	while(total_sequence < length) {
+		sequence_size = std::min(length-total_sequence, STREAM_SIZE);
+		total_sequence += sequence_size;
+
+		if (rank == 0) {
+			for (int j = 0, i = 0; j < sequence_size; j++, i+=2) {
+				sequence[i] = random(0, nkeys-1);
+				sequence[i+1] = random(0, nkeys-1);
+				
+				if (sequence[i] == sequence[i+1])
+					sequence[i] = (sequence[i] + 1) % nkeys;
+			}	
+		}
+
+		MPI_Ibcast(&sequence, sequence_size * 2, MPI_LONG, 0, MPI_COMM_WORLD, &ibcast_request);
+		if (rank != 0) MPI_Wait(&ibcast_request, MPI_STATUS_IGNORE);
+
+		for (int j = 0, i = 0; j < sequence_size; j++, i+=2) {
+			key1 = sequence[i];
+			key2 = sequence[i+1];
+
+			map[key1]++;
+			map[key2]++;
+
+			if (setKeys[rank].contains(key1)) {
+				if (map[key1] == SIZE) {
+					if (map[key2] > SIZE) {
+						count = map[key2] - SIZE;
+
+						while (true) {
+							MPI_Request req;
+							MPI_Irecv(&map.data()[key2], 1, MPI_LONG, findOwner(setKeys, key2), key2, MPI_COMM_WORLD,&req);
+							MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+							if (count > (SIZE - map[key2])) count -= (SIZE - map[key2]);
+							else break;
+						}
+
+						map[key2] += count;
+					}
+					
+					if (map[key2] != 0) {
+						r1 = compute(map[key1], map[key2], key1, key2);
+						V[key1] += r1;
+
+						auto _r1 = static_cast<unsigned long>(r1) % SIZE;
+						newKey1 = (_r1>(SIZE/2)) ? 0 : _r1;
+						resetkey1 = true;
+
+						for (int dest = 0, i = 0; dest < size; dest++)
+							if (dest != rank)
+								MPI_Isend(&newKey1, 1, MPI_LONG, dest, key1, MPI_COMM_WORLD, &request_key1.data()[i++]);
+					}
+				}
+			}
+
+			if (setKeys[rank].contains(key2)) {
+				if (map[key2] == SIZE) {
+					if (map[key1] > SIZE) {
+						count = map[key1] - SIZE;
+
+						while (true) {
+							MPI_Request req;
+							MPI_Irecv(&map.data()[key1], 1, MPI_LONG, findOwner(setKeys, key1), key1, MPI_COMM_WORLD,&req);
+							MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+							if (count > (SIZE - map[key1])) count -= (SIZE - map[key1]);
+							else break;
+						}
+						map[key1] += count;
+					}
+					
+					if (map[key1] != 0) {
+						r2 = compute(map[key2], map[key1], key2, key1);
+						V[key2] += r2;
+
+						auto _r2 = static_cast<unsigned long>(r2) % SIZE;
+						map[key2] = (_r2>(SIZE/2)) ? 0 : _r2;
+						resetkey2 = true;
+
+						for (int dest = 0, i = 0; dest < size; dest++)
+							if (dest != rank)
+								MPI_Isend(&map.data()[key2], 1, MPI_LONG, dest, key2, MPI_COMM_WORLD, &request_key2.data()[i++]);
+					}
+				}
+			}
+
+			if (resetkey1) {
+				MPI_Waitall(request_key1.size(), request_key1.data(), MPI_STATUSES_IGNORE);
+				map[key1] = newKey1;
+				resetkey1 = false;
+			}
+
+			if (resetkey2) {
+				MPI_Waitall(request_key2.size(), request_key2.data(), MPI_STATUSES_IGNORE);
+				resetkey2 = false;
+			}
+		}
+
+		if (rank == 0) MPI_Wait(&ibcast_request, MPI_STATUS_IGNORE);
+	}
+
+	// compute the last values
+	for(long i = 0; i < nkeys; i++) {
+		for(long j = 0; j < nkeys; j++) {
+			if (i == j) continue;
+
+			if (setKeys[rank].contains(i)) {
+				if (map[i] > 0) {
+					if (!setKeys[rank].contains(j) && (map[j] >= SIZE)) {
+						count = map[j] - SIZE;
+
+						while (true) {
+							MPI_Request req;
+							MPI_Irecv(&map.data()[j], 1, MPI_LONG, findOwner(setKeys, j), j, MPI_COMM_WORLD, &req);
+							MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+							if (count >= (SIZE - map[j])) count -= (SIZE - map[j]);
+							else break;
+						}
+					
+						map[j] += count;
+					}
+
+					if (map[j] > 0)
+						V[i] += compute(map[i], map[j], i, j);
+				}
+			}
+
+			if (setKeys[rank].contains(j)) {
+				if (map[j] > 0) {
+					if (!setKeys[rank].contains(i) && (map[i] >= SIZE)) {
+						count = map[i] - SIZE;
+
+						while (true) {
+							MPI_Request req;
+							MPI_Irecv(&map.data()[i], 1, MPI_LONG, findOwner(setKeys, i), i, MPI_COMM_WORLD, &req);
+							MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+							if (count >= (SIZE - map[i])) count -= (SIZE - map[i]);
+							else break;
+						}
+					
+						map[i] += count;
+					}
+
+					if (map[i] > 0)
+						V[j] += compute(map[j], map[i], j, i);
+				}
+			}
+		}
+	}
+
+	MPI_Finalize();
+
+	gettimeofday(&wt1, NULL);
+	if (rank == 0) std::printf("Total time: %f (S)\n", diffmsec(wt1, wt0)/1000);
+
+	// printing the results
+	if (print)
+		for(auto key : V)
+	 		std::printf("key %ld : %f\n", key.first, key.second);
+}
--- Assignment4/nkeys.cpp
+++ Assignment4/nkeys.cpp
@@ -2,10 +2,9 @@
 #include <random>
 #include <map>
 #include <vector>
-#include <string>
 #include <sys/time.h>
 
-const long   SIZE = 64;
+const long SIZE = 64;
 
 double diffmsec(const struct timeval &a, const struct timeval &b) {
 	long sec  = (a.tv_sec  - b.tv_sec);
@@ -23,19 +22,17 @@ long random(const int &min, const int &max) {
 	static std::mt19937 generator(117);
 	std::uniform_int_distribution<long> distribution(min,max);
 	return distribution(generator);
-};		
-
+}	
 
 void init(auto& M, const long c1, const long c2, const long key) {
-	for(long i=0;i<c1;++i)
-		for(long j=0;j<c2;++j)
+	for(long i = 0; i < c1; i++)
+		for(long j = 0; j < c2; j++)
 			M[i][j] = (key-i-j)/static_cast<float>(SIZE);
 }
 
 // matrix multiplication:  C = A x B  A[c1][c2] B[c2][c1] C[c1][c1]
 // mm returns the sum of the elements of the C matrix
-auto mm(const auto& A, const auto& B, const long c1,const long c2) {
-
+auto mm(const auto& A, const auto& B, const long c1, const long c2) {
 	float sum{0};
 	
     for (long i = 0; i < c1; i++) {
@@ -46,6 +43,7 @@ auto mm(const auto& A, const auto& B, const long c1,const long c2) {
             sum += accum;
 		}
 	}
+
 	return sum;
 }
 
@@ -53,7 +51,6 @@ auto mm(const auto& A, const auto& B, const long c1,const long c2) {
 // and execute a matrix multiplication between the two matrices
 // to obtain the sum of the elements of the result matrix 
 float compute(const long c1, const long c2, long key1, long key2) {
-
 	std::vector<std::vector<float>> A(c1, std::vector<float>(c2,0.0));
 	std::vector<std::vector<float>> B(c2, std::vector<float>(c1,0.0));
 
@@ -63,70 +60,68 @@ float compute(const long c1, const long c2, long key1, long key2) {
 	return r;
 }
 
-
-
 int main(int argc, char* argv[]) {
+	struct timeval wt1, wt0;
+	gettimeofday(&wt0, NULL);
+
 	if (argc < 3) {
 		std::printf("use: %s nkeys length [print(0|1)]\n", argv[0]);
 		std::printf("     print: 0 disabled, 1 enabled\n");
 		return -1;
 	}
 	
-	long nkeys  = std::stol(argv[1]);  // total number of keys
-	// length is the "stream length", i.e., the number of random key pairs
-	// generated
-	long length = std::stol(argv[2]);  
+	long nkeys  = std::stol(argv[1]); // total number of keys
+	long length = std::stol(argv[2]); // length is the "stream length", i.e., the number of random key pairs generated
 	bool print = false;
 	if (argc == 4)
 		print = (std::stoi(argv[3]) == 1) ? true : false;
 	
-	struct timeval wt1,wt0;
-
-	gettimeofday(&wt0, NULL);
-
 	long key1, key2;
 
 	std::map<long, long> map;
-	for(long i=0;i<nkeys; ++i) map[i]=0;
+	for(long i = 0; i < nkeys; i++) map[i]=0;
 	
 	std::vector<float> V(nkeys, 0);
 	bool resetkey1=false;
 	bool resetkey2=false;
-	for(int i=0;i<length; ++i) {
+
+	for(int i = 0; i < length; i++) {
 		key1 = random(0, nkeys-1);  // value in [0,nkeys[
 		key2 = random(0, nkeys-1);  // value in [0,nkeys[
 		
 		if (key1 == key2) // only distinct values in the pair
-			key1 = (key1+1) % nkeys; 
-
+			key1 = (key1+1) % nkeys;
+		
 		map[key1]++;  // count the number of key1 keys
 		map[key2]++;  // count the number of key2 keys
 
 		float r1;
 		float r2;
+		
 		// if key1 reaches the SIZE limit, then do the computation and then
 		// reset the counter ....
-		if (map[key1] == SIZE && map[key2]!=0) {			
+		if (map[key1] == SIZE && map[key2]!=0) {		
 			r1= compute(map[key1], map[key2], key1, key2);
 			V[key1] += r1;  // sum the partial values for key1
-			resetkey1=true;			
+			resetkey1 = true;			
 		}
+
 		// if key2 reaches the SIZE limit ....
-		if (map[key2] == SIZE && map[key1]!=0) {			
+		if (map[key2] == SIZE && map[key1]!=0) {		
 			r2= compute(map[key2], map[key1], key2, key1);
 			V[key2] += r2;  // sum the partial values for key1
 			resetkey2=true;
 		}
+
 		if (resetkey1) {
-			// updating the map[key1] initial value before restarting
-			// the computation
+			// updating the map[key1] initial value before restarting the computation
 			auto _r1 = static_cast<unsigned long>(r1) % SIZE;
 			map[key1] = (_r1>(SIZE/2)) ? 0 : _r1;
 			resetkey1 = false;
 		}
+
 		if (resetkey2) {
-			// updating the map[key2] initial value before restarting
-			// the computation
+			// updating the map[key2] initial value before restarting the computation
 			auto _r2 = static_cast<unsigned long>(r2) % SIZE;
 			map[key2] = (_r2>(SIZE/2)) ? 0 : _r2;
 			resetkey2 = false;
@@ -134,10 +129,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	// compute the last values
-	for(long i=0;i<nkeys; ++i) {
-		for(long j=0;j<nkeys; ++j) {
+	for(long i = 0; i < nkeys; i++) {
+		for(long j = 0; j < nkeys; j++) {
 			if (i==j) continue;
+
 			if (map[i]>0 && map[j]>0) {
+				
 				auto r1= compute(map[i], map[j], i, j);
 				auto r2= compute(map[j], map[i], j, i);
 				V[i] += r1;
@@ -147,12 +144,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	gettimeofday(&wt1, NULL);
-
 	std::printf("Total time: %f (S)\n", diffmsec(wt1, wt0)/1000);
 
 	// printing the results
-	if (print) {
+	if (print)
 		for(long i=0;i<nkeys; ++i)
 			std::printf("key %ld : %f\n", i, V[i]);
-	}
 }
